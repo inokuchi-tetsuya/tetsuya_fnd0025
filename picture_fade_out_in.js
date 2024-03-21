43603935fedc53b1297d881f9a52a4895ccf5d3d
inokuchi-tetsuya <159498291+inokuchi-tetsuya@users.noreply.github.com>
2024-03-21 16:55:11 +0900

    Update picture_fade_out_in.js

--- picture_fade_out_in.js
+++ picture_fade_out_in.js
@@ -1,20 +1,16 @@
 'use strict'
 // // 1行目に記載している 'use strict' は削除しないでください
 
-let intervalId;
-
 let image = document.getElementById('dietMan');
 let flag = true;
-
+changeImage();
 // 3秒ごとにchangeImage()関数を実行 
-setInterval(changeImage, 3000);
-
+setInterval(changeImage, 3000); //setInterval 関数は、指定した間隔ごとに指定した関数を繰り返し実行
 
 function changeImage(){  
-  if(image.style.opacity == ''){
+  if(image.style.opacity === ''){
     image.style.opacity = 1;
   }
-
   let opacityInt = image.style.opacity * 100;
   //フェードアウトの処理（opacityを50ミリ秒ごとに0.05づつ減らす）
   let intervalId = setInterval( () => {
